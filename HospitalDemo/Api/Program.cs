6250b92ad6630f9a956e473a7bbd2509e0c9cfc3
SapereAudePr <capitolsmife@gmail.com>
2026-04-15 15:06:13 +0300

    MinimalApi project created

--- HospitalDemo/Api/Program.cs
+++ HospitalDemo/Api/Program.cs
@@ -9,12 +9,13 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddControllers();
+        builder.Services.AddAuthorization();
+
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
-        
-        builder.Services.AddInfrastructure(builder.Configuration);
 
+        builder.Services.AddInfrastructure(builder.Configuration);
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -27,8 +28,24 @@ public class Program
 
         app.UseAuthorization();
 
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-        app.MapControllers();
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
 
         app.Run();
     }
